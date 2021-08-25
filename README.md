c76c3bf839b558af96a9c3e1618afa114f9ab588
Sophia Stephenson <stephenson3@wisc.edu>
2021-08-24 20:21:59 -0500

    first pass at file structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# workout-tracker
+# Workout Tracker
 
 ## Description
 
---
+++ models/workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for the workout",
+  },
+  value: {
+    type: Number,
+    required: "Enter a weight",
+  },
+  value: {
+    type: String,
+    trim: true,
+    required: "Enter workout length",
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Transaction;
